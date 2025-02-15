514f7f1b95d672446124b4c88765ce4ab0ce90c8
kishanghosh090 <kishanghosh090@gmail.com>
2025-02-15 16:43:33 +0530

    added

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+// register controller
+const register = (req, res, next) => {};
+
+// login controller
+const login = (req, res, next) => {};
+
+// google login
+const googleLogin = (req, res, next) => {};
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,46 +1,53 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const UserSchema = new Schema ({
-    userName: {
-        type: String,
-        required: true 
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: String, // Please Correct Here
+    default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  },
+  dob: {
+    type: Date,
+  },
+  Gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"], // Is that correct or should we add radio buttons only
+  },
+  phone: {
+    type: Number,
+    required: true, // Take a look here
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectsId,
+      ref: "UserSchema",
     },
-    email: {
-        type: String,
-        required: true 
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserSchema",
     },
-    password: {
-        type: String,
-        required: true 
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Type.ObjectId,
+      ref: "UserSchema",
+      required: true,
     },
-    profilePic: {
-        type: String, // Please Correct Here
-    },
-    dob: {
-        type: Date,
-        required: true 
-    },
-    Gender: {
-        type: String,
-        enum: ['Male', 'Female', 'Other'], // Is that correct or should we add radio buttons only
-        required: true
-    },
-    phone: {
-        type: Number,
-        required: true // Take a look here
-    },
-    followers: [{
-        type: mongoose.Schema.Types.ObjectsId,
-        ref: 'UserSchema'
-    }],
-    following: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'UserSchema'
-    }],
-    posts: [{
-        type: mongoose.Schema.Type.ObjectId,
-        ref: 'UserSchema',
-        required: true 
-    }]
-})
+  ],
+});
+
+export const User = mongoose.model("User", UserSchema);
