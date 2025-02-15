5bdb05ebda3a723ced831034fab6b5f52d27e02a
kishanghosh090 <kishanghosh090@gmail.com>
2025-02-15 22:13:20 +0530

    added

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+// routes middlewares
+import userRoute from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoute);
+
 export { app };
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,19 @@
 // register controller
-const register = (req, res, next) => {};
+const register = (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
 
 // login controller
 const login = (req, res, next) => {};
 
 // google login
 const googleLogin = (req, res, next) => {};
+
+// edit profile
+const editProfile = (req, res, next) => {};
+
+export { register, login, googleLogin, editProfile };
